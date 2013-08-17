420f1327b51a54c6c059cacdd6cd542056abb6fe
Shuai Yuan <shuaisyn@gmail.com>
2013-08-17 09:35:14 +0800

    add forum page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Highand::Application.routes.draw do
   root :to => 'pages#home'
   get 'about' => 'pages#about'
+  get 'forum' => 'pages#forum'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
